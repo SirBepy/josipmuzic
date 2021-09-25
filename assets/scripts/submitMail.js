2b765716e358c80c6382d1590d106c39150a460e
SirBepy <josipmuzic99@gmail.com>
2021-09-25 18:28:58 +0200

    Add contact form submission logic

--- assets/scripts/submitMail.js
+++ assets/scripts/submitMail.js
@@ -1,30 +1,22 @@
-// TODO: Add logic from smth like "simple javascript spredsheet form submission"
-// e.g. https://dev.to/omerlahav/submit-a-form-to-a-google-spreadsheet-1bia
-
-
-const SPREADSHEET_ID = '12FRjg3LepFIrx41fOwaFputQxDnp_HXm5_OZbA5HrBg';
-const CLIENT_ID = '798815957955-t5jaib26pksjmlbdrvh6esqu9aa7qgdi.apps.googleusercontent.com';
-const API_KEY = 'AIzaSyAK-ihaYiUpQ4QtWwBhqJWFCS8QsQ7qwEY';
-const SCOPE = 'https://www.googleapis.com/auth/spreadsheets';
-
-onFormSubmit(valuesForSheet) {
-    const params = {
-        spreadSheetId: SPREADSHEET_ID,
-        range: 'Sheet1',
-        valueInputOption: 'RAW',
-        insertDataOptions: 'INSERT_ROWS',
-    };
-
-    const valueRangeBody = {
-        'majorDimension': 'ROWS',
-        'values': [valuesForSheet]
-    };
-
-    let request = gapi.client.sheets.spreadsheets.values.append(params, valueRangeBody);
-    request.then(function(response) {
-        //TODO: insert response behaviour on submission
-        console.log(response.result);
-    }, function(reason) {
-        console.error('error: ' + reason.result.error.message);
+async function onFormSubmit(event) {
+  event.preventDefault();
+  const status = document.getElementById("my-form-status");
+  const data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+        // TODO: Add success handler
+      form.reset();
+    })
+    .catch((error) => {
+        // TODO: Add fail handler
     });
 }
+
+const form = document.getElementById("contact-form");
+form.addEventListener("submit", onFormSubmit);
